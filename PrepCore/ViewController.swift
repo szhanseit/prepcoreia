061ef73259bd535804396bd33207cb96309950b8
szhanseit <sultan.zhanseit@mynbps.org>
2020-10-30 12:08:39 -0400

    Initial commit

---
+++ PrepCore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrepCore
+//
+//  Created by Sultan Zhanseit on 10/26/20.
+//  Copyright © 2020 Sultan Zhanseit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
